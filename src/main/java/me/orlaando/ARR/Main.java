421e3463303d81b3fcbeb473decc96fb5b7a40d8
Orlaando-dev <xorlxndo@outlook.com>
2020-09-05 14:56:20 -0600

    Load NMSAdapter onEnable

--- src/main/java/me/orlaando/ARR/Main.java
+++ src/main/java/me/orlaando/ARR/Main.java
@@ -1,6 +1,7 @@
 package me.orlaando.ARR;
 
 import co.aikar.commands.PaperCommandManager;
+import me.orlaando.ARR.adapters.*;
 import me.orlaando.ARR.commands.CommandARR;
 import me.orlaando.ARR.configuration.MessageHandler;
 import me.orlaando.ARR.listeners.InteractionListener;
@@ -19,10 +20,15 @@ public class Main extends JavaPlugin {
     private static MessageHandler messageHandler;
     private PaperCommandManager commandManager;
 
+    // since the proper NMS class was assigned onEnable, we are now backwards compatible!
+    private static NMSAdapter nmsAdapter;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     @Override public void onEnable() {
         ARR = this;
+
+        setupNMS();
 
         messageHandler = new MessageHandler(ARR);
 
@@ -48,6 +54,35 @@ public class Main extends JavaPlugin {
         getSqLiteStorage().stopStorage();
     }
 
+    public void setupNMS() {
+        String version;
+        try {
+            version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            LOGGER.error("You aren't using a supported Mincraft version..");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        LOGGER.info("Your server is running version " + version);
+
+        switch (version) {
+            case "v1_16_R2":
+                nmsAdapter = new mc1_16_2();
+            case "v1_15_R1":
+                nmsAdapter = new mc1_15_2();
+            case "v1_14_R1":
+                nmsAdapter = new mc1_14_4();
+            case "v1_13_R2":
+                nmsAdapter = new mc1_13_2();
+        }
+
+        if (version == null) {
+            LOGGER.error("You aren't using a supported Mincraft version..");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
+
     public void reloadMessages() {
         messageHandler = new MessageHandler(ARR);
     }
@@ -62,6 +97,10 @@ public class Main extends JavaPlugin {
 
     @NotNull public static Main getARR() {
         return ARR;
+    }
+
+    @NotNull public static NMSAdapter getNmsAdapter() {
+        return nmsAdapter;
     }
 
 
