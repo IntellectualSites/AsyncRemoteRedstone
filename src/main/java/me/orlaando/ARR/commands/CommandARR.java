66a63cd8c2c2d7f74ebb78a40bba0fce07d24633
Orlaando-dev <xorlxndo@outlook.com>
2020-09-04 22:11:55 -0600

    Add /arr command

---
+++ src/main/java/me/orlaando/ARR/commands/CommandARR.java
@@ -0,0 +1,39 @@
+package me.orlaando.ARR.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.*;
+import me.orlaando.ARR.Main;
+import me.orlaando.ARR.configuration.TranslatableMessage;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+@CommandAlias("arr")
+@CommandPermission("arr.plugin")
+public class CommandARR extends BaseCommand {
+
+    @Subcommand("reload")
+    @CommandPermission("arr.reload")
+    public void reload(CommandSender sender) {
+        try {
+            Main.getARR().reloadConfig();
+            Main.getARR().reloadMessages();
+            sender.sendMessage(TranslatableMessage.of("plugin.reload.success"));
+        } catch (final Exception e) {
+            sender.sendMessage(TranslatableMessage.of("plugin.reload.failure"));
+            e.printStackTrace();
+        }
+    }
+
+    @CatchUnknown
+    @HelpCommand
+    @Default
+    public void onUnknown(CommandSender sender) {
+        sender.sendMessage(
+                String.format(
+                        TranslatableMessage.of("command.syntax"),
+                        "/arr reload")
+        );
+    }
+
+
+}
